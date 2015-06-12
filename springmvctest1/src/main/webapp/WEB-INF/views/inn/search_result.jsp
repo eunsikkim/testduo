b4a2b7e26c35e550705ba32cd04c1c982d5abb3e
eunsik <eunsik7887@gmail.com>
2015-06-13 01:39:27 +0900

    검색결과페이지수정

--- springmvctest1/src/main/webapp/WEB-INF/views/inn/search_result.jsp
+++ springmvctest1/src/main/webapp/WEB-INF/views/inn/search_result.jsp
@@ -1,15 +1,58 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
 <%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
-<c:choose>
+<%-- <c:choose>
 <c:when test="${requestScope.list.size()==0}" >
 검색실패
 </c:when>
 <c:otherwise>
 <c:forEach var="list" items="${requestScope.list}">
-${list.innName }  / ${list.innCity } / ${list.innArea } / ${list.innAddress } / <%--  ${list.innAcceptableNo } --%> / ${list.innPrice } / ${list.innInfo }<br>
+${list.innName }  / ${list.innCity } / ${list.innArea } / ${list.innAddress } / ${list.acceptableNo } / ${list.innPrice } / ${list.innInfo }<br>
 </c:forEach>
 </c:otherwise>
-</c:choose>
+</c:choose> --%>
 
+<div class="section">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-12">
+				<table class="table">
+					<thead>
+						<tr>
+							<th >숙소 번호</th>
+							<th>숙소명</th>
+							<th>지역(대)</th>
+							<th>지역(중)</th>
+							<th>상세 주소</th>
+							<th>유형</th>
+							<th>수용가능인원</th>
+							<th>가격</th>
+						</tr>
+					</thead>
+					<tbody>
+					<c:choose>
+					<c:when test="${requestScope.list.size()==0}" >
+					<tr align="center"><td colspan="8">검색결과가 존재하지 않습니다.</td></tr>
+					</c:when>
+					<c:otherwise>
+						<c:forEach var="list" items="${requestScope.list}">
+							<tr>
+								<td><!-- <div id="showInnNo"></div> -->${list.innNo }</td>
+								<td><!-- <div id="showInnName"></div> -->${list.innName }</td>
+								<td><!-- <div id="showInnCity"></div> -->${list.innCity }</td>
+								<td><!-- <div id="showInnArea"></div> -->${list.innArea }</td>
+								<td><!-- <div id="showInnInfo"></div> -->${list.innAddress }</td>
+								<td><!-- <div id="showInnType"></div> -->${list.innType }</td>
+								<td><!-- <div id="showInnAcceptable"></div> -->${list.acceptableNo }</td>
+								<td><!-- <div id="showInnPrice"></div> -->${list.innPrice }</td>
+							</tr>
+						</c:forEach>
+					</c:otherwise>
+				</c:choose>
+					</tbody>
+				</table>
+			</div>
+		</div>
+	</div>
+</div>
 
--- springmvctest1/target/test-classes/insertValues.sql
+++ springmvctest1/target/test-classes/insertValues.sql
@@ -28,6 +28,7 @@ inn_price, inn_info,inn_availability,member_id)
 values(inn_sequence.nextval,'영통메르디앙4','수원','영통','영통메르디앙APT4','집 전체',3,27000,'경희대학교 인접해있습니다.','Y','oldtype');
 
 select*from inn;
+select*from AVAILABLEDATE
 
 insert into amenity(amenity_wifi,amenity_bed,amenity_tv,amenity_kitchen,amenity_bbq, amenity_no) values
 ('Y','Y','Y','Y','Y',1);
@@ -36,15 +37,15 @@ insert into amenity(amenity_wifi,amenity_bed,amenity_tv,amenity_kitchen,amenity_
 insert into amenity(amenity_wifi,amenity_bed,amenity_tv,amenity_kitchen,amenity_bbq, amenity_no) values
 ('Y','N','N','N','Y',3);
 insert into amenity(amenity_wifi,amenity_bed,amenity_tv,amenity_kitchen,amenity_bbq, amenity_no) values
-('Y','N','N','N','Y',4);
+('Y','N','N','N','Y',21);
 
 insert into availabledate(availabledate_no, inn_no, availabledate_st, availabledate_end) values(availabledate_no_sequence.nextval, 1, '2015-05-18', '2015-05-25');
 insert into availabledate(availabledate_no, inn_no, availabledate_st, availabledate_end) values(availabledate_no_sequence.nextval, 1, '2015-05-27', '2015-05-31');
 insert into availabledate(availabledate_no, inn_no, availabledate_st, availabledate_end) values(availabledate_no_sequence.nextval, 2, '2015-05-20', '2015-05-24');
 insert into availabledate(availabledate_no, inn_no, availabledate_st, availabledate_end) values(availabledate_no_sequence.nextval, 3, '2015-05-13', '2015-05-22');
 insert into availabledate(availabledate_no, inn_no, availabledate_st, availabledate_end) values(availabledate_no_sequence.nextval, 3, '2015-06-02', '2015-06-10');
-insert into availabledate(availabledate_no, inn_no, availabledate_st, availabledate_end) values(availabledate_no_sequence.nextval, 5, '2015-05-27', '2015-05-31');
-insert into availabledate(availabledate_no, inn_no, availabledate_st, availabledate_end) values(availabledate_no_sequence.nextval, 5, '2015-05-27', '2015-05-31');
-insert into availabledate(availabledate_no, inn_no, availabledate_st, availabledate_end) values(availabledate_no_sequence.nextval, 6, '2015-05-27', '2015-05-31');
-insert into availabledate(availabledate_no, inn_no, availabledate_st, availabledate_end) values(availabledate_no_sequence.nextval, 6, '2015-05-27', '2015-05-31');
+insert into availabledate(availabledate_no, inn_no, availabledate_st, availabledate_end) values(availabledate_no_sequence.nextval, 21, '2015-05-27', '2015-05-31');
+insert into availabledate(availabledate_no, inn_no, availabledate_st, availabledate_end) values(availabledate_no_sequence.nextval, 21, '2015-05-27', '2015-05-31');
+insert into availabledate(availabledate_no, inn_no, availabledate_st, availabledate_end) values(availabledate_no_sequence.nextval, 22, '2015-05-27', '2015-05-31');
+insert into availabledate(availabledate_no, inn_no, availabledate_st, availabledate_end) values(availabledate_no_sequence.nextval, 22, '2015-06-01', '2015-06-07');
 insert into availabledate(availabledate_no, inn_no, availabledate_st, availabledate_end) values(availabledate_no_sequence.nextval, 7, '2015-05-27', '2015-05-31');
